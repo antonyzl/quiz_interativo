1b5dac6e2c7d64526a28e22f95d138a9bedd989b
antonyzl <antony.fernandes.oliveira@escola.pr.gov.br>
2025-08-27 07:43:56 -0300

    Update pergunta.js

--- js/pergunta.js
+++ js/pergunta.js
@@ -1,1 +1,97 @@
-
+const perguntas = [
+  {
+      enunciado: "Quem ganharia em uma luta sem armas, 1 Gorila ou 100 humanos",
+    alternativas: [
+      {
+        texto: "Gorila",
+        afirmacao: [
+          "Poxa, eu ja acho que voce está errado",
+          "Você tem certeza?"
+        ],
+      },
+      {
+        texto: "100 humanos",
+        afirmacao: [
+          "Parabens, eu estou com você nessa!",
+          "Eu tambem concordo!"
+        ],
+      }
+    ]
+  },
+    {
+      enunciado: "Voce prefere, azeitona com ou sem caroço?",
+    alternativas: [
+      {
+        texto: "com caroço",
+        afirmacao: [
+          "Eu ja nao gosto de azeitona, mas discordo",
+          "afirmação 2"
+        ],
+      },
+      {
+        texto: "alternativa 2",
+        afirmacao: [
+          "afirmação 1",
+          "afirmação 2"
+        ],
+      }
+    ]
+  },
+    {
+      enunciado: "Pergunta 1",
+    alternativas: [
+      {
+        texto: "alternativa 1",
+        afirmacao: [
+          "afirmação 1",
+          "afirmação 2"
+        ],
+      },
+      {
+        texto: "alternativa 2",
+        afirmacao: [
+          "afirmação 1",
+          "afirmação 2"
+        ],
+      }
+    ]
+  },
+    {
+      enunciado: "Pergunta 1",
+    alternativas: [
+      {
+        texto: "alternativa 1",
+        afirmacao: [
+          "afirmação 1",
+          "afirmação 2"
+        ],
+      },
+      {
+        texto: "alternativa 2",
+        afirmacao: [
+          "afirmação 1",
+          "afirmação 2"
+        ],
+      }
+    ]
+  },
+    {
+      enunciado: "Pergunta 1",
+    alternativas: [
+      {
+        texto: "alternativa 1",
+        afirmacao: [
+          "afirmação 1",
+          "afirmação 2"
+        ],
+      },
+      {
+        texto: "alternativa 2",
+        afirmacao: [
+          "afirmação 1",
+          "afirmação 2"
+        ],
+      }
+    ]
+  }
+]
