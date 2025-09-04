c3084cbdefeda6cacf48ab6076aa8307defa9c99
antonyzl <antony.fernandes.oliveira@escola.pr.gov.br>
2025-09-04 12:04:46 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,1 +1,8 @@
+const nomes = ["Antony", "Lucas", "Ruan", "Fernando", "Willian", "Nicollas"];
 
+function aleatorio(lista){
+  const posicao = Math.floor(Math.random()* lista.length);
+  return lista(position);
+}
+
+const nome = aleatorio(nomes)
