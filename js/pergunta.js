a922e5672ed8c508057d3404ad46683729938bb9
antonyzl <antony.fernandes.oliveira@escola.pr.gov.br>
2025-09-15 10:27:48 -0300

    Update pergunta.js

--- js/pergunta.js
+++ js/pergunta.js
@@ -1,19 +1,20 @@
-const perguntas = [
+export const perguntas = [
   {
       enunciado: "Você vai entrar na floresta ou vai ficar em casa?",
     alternativas: [
       {
         texto: "Entrar na floresta",
         afirmacao: [
-          "",
-          ""
+          "Voce entrou na floresta!",
+          "Voce escolheu floresta!"
         ],
+        proximo: 1,
       },
       {
         texto: "Ficar em casa",
         afirmacao: [
-          "",
-          ""
+          "Você sobreviveu!",
+          "Você sobreviveu!"
         ],
       }
     ]
@@ -24,16 +25,18 @@ const perguntas = [
       {
         texto: "Caminho de pedras",
         afirmacao: [
-          "",
-          ""
+          "Voce preferiu as pedras!",
+          "Voce preferiu as pedras!"
         ],
-      },
+        proximo: 2,
+      };
       {
         texto: "Caminho de terra",
         afirmacao: [
-          "",
-          ""
+          "Terra foi sua escolha",
+          "Terra foi sua escolha"
         ],
+          proximo: 2,
       }
     ]
   },
@@ -43,35 +46,37 @@ const perguntas = [
       {
         texto: "Correr",
         afirmacao: [
-          "",
-          ""
+          "Voce zarpou!",
+          "Voce correu!"
         ],
+        proximo: 3,
       },
       {
         texto: "Se esconder",
         afirmacao: [
-          "",
-          ""
+          "Você se escondeu muito bem, e sobreviveu!",
+          "Você se escondeu muito bem, e sobreviveu!"
         ],
       }
     ]
   },
     {
-      enunciado: "Você encontrou uma cabana. Vai bater na porta ou espiar pela janela?"
+      enunciado: "Você foi pelas pedras e encontrou uma cabana. Vai bater na porta ou espiar pela janela?"
     alternativas: [
       {
         texto: "Bater na porta",
         afirmacao: [
-          "",
-          ""
+          "Você conseguiu um abrigo e sobreviveu!",
+          "Você conseguiu um abrigo e sobreviveu!"
         ],
       },
       {
         texto: "Espiar pela janela",
         afirmacao: [
-          ""
-          ""
+          "Voce preferiu bizoiar"
+          "Voce escolheu ver pela janela"
         ],
+        proximo: 4,
       }
     ]
   },
@@ -81,15 +86,36 @@ const perguntas = [
       {
         texto: "Falar com ela",
         afirmacao: [
-          "",
-          ""
+          "Voce quis falar com ela",
+          "Voce decidiu falar com a pessoa"
         ],
+        proxima: 5,
       },
       {
         texto: "Ir embora",
         afirmacao: [
+          "Você voltou pra casa com segurança!",
+          "Você voltou pra casa com segurança!"
+        ],
+      }
+
+      
+    {
+      enunciado: "Você viu uma pessoa lá dentro. Vai falar com ela ou vai embora?"
+    alternativas: [
+      {
+        texto: "Falar com ela",
+        afirmacao: [
           "",
           ""
+        ],
+        proxima: 5,
+      },
+      {
+        texto: "Ir embora",
+        afirmacao: [
+          "Você voltou pra casa com segurança!",
+          "Você voltou pra casa com segurança!"
         ],
       }
     ]
